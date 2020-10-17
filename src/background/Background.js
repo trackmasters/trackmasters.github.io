8831a14d5cdce1ae048c8ecca139ccaa64076bb4
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2020-10-17 23:29:45 +0200

    Basic responsivity

--- src/background/Background.js
+++ src/background/Background.js
@@ -7,8 +7,17 @@ export default function Background () {
 	const text = useText(introduction);
 
 	return (
-		<section id="background" className="uk-background-fixed uk-background-top-center uk-background-norepeat uk-background-cover
-			uk-height-viewport uk-width-viewport uk-flex uk-flex-top uk-background-blend-hard-light uk-background-primary">
+		<section id="background" className="
+		uk-background-fixed
+		uk-background-top-center
+		uk-background-norepeat
+		uk-background-cover
+		uk-height-viewport
+		uk-width-viewport
+		uk-flex
+		uk-flex-top
+		uk-background-blend-hard-light
+		uk-background-primary">
 			<h1>{text}</h1>
 		</section>
 	);
