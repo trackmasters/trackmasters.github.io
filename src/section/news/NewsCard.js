22bc98d585334ca925ca858ba465d4c3549cdf8a
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2020-11-15 21:49:53 +0100

    News

--- src/section/news/NewsCard.js
+++ src/section/news/NewsCard.js
@@ -5,12 +5,14 @@ export default class NewsCard extends React.Component {
 	render() {
 		return (
 			<div className="uk-card uk-card-hover uk-card-default">
-				<div className="uk-card-media-top">
-					<img src={this.props.image} alt=""/>
+				<div className="uk-card-media-top" style={{'overflow':'hidden', 'height':'200px'}}>
+					<img src={this.props.image} style={{'object-fit': 'cover', 'height':'100%', 'width':'100%'}} alt=""/>
 				</div>
-				<div className="uk-card-body" style={{'padding': '20px'}}>
+				<div className="uk-card-body" style={{'height':'128px','padding': '20px'}}>
 					<h3 className="uk-card-title">{this.props.label}</h3>
-					<p>{this.truncate(this.props.text, 20)}</p>
+					<p>{this.truncate(this.props.text, 200)}</p>
+				</div>
+				<div className="uk-card-body" style={{'height':'50px', 'margin-top':'50px', 'padding': '20px'}}>
 					<hr/>
 					<p className="uk-align-left uk-margin-remove">{this.props.author}</p>
 					<p className="uk-align-right uk-margin-remove">{this.props.date}</p>
