30b95235d1653c62c1c5c4f12cc82af937c353aa
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2020-11-22 23:08:28 +0100

    Kenburns on scrollspy

--- src/section/aboutRacing/AboutRacing.js
+++ src/section/aboutRacing/AboutRacing.js
@@ -10,7 +10,8 @@ export default function AboutRacing(props) {
 		<section id={props.id}
 				 className="uk-section uk-inline uk-section-default uk-padding-remove uk-height-viewport uk-width-viewport uk-overflow-hidden">
 			<div id="AboutRacingBackground"
-				 className="uk-container  uk-container-large uk-height-viewport uk-width-viewport uk-text-center uk-background-cover uk-padding-remove-top uk-animation-kenburns uk-animation-reverse"/>
+				 className="uk-container  uk-container-large uk-height-viewport uk-width-viewport uk-text-center uk-background-cover uk-padding-remove-top"
+				 data-uk-scrollspy="cls: uk-animation-kenburns uk-animation-reverse; repeat: true"/>
 			<div className="uk-position-cover uk-overlay uk-overlay-primary">
 				<div style={{ 'marginTop': '57px' }}>
 					<SectionLabel>O závodu</SectionLabel>
