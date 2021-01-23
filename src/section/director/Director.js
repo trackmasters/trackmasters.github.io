32e0742883e167c8cf1b8ae9e7e8cbea155891b4
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2021-01-23 20:12:58 +0100

    Responsive v6 - director

--- src/section/director/Director.js
+++ src/section/director/Director.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import './Director.scss';
 import denis from "../../content/director/denis.jpg"
 
 import data from "../../content/director/director.json";
@@ -9,18 +10,16 @@ function Director(props) {
 
     return <Section id="Director" feel={props.feel} label={props.label}>
         <div className="uk-container uk-section uk-padding-remove-top uk-padding-remove-bottom">
-            <div className="uk-grid uk-grid-large"
+            <div className="uk-text-center"
                  data-uk-scrollspy="target: > div; delay: 150; cls: uk-animation-slide-bottom-medium"
                  data-uk-grid="">
-
-                <div className="uk-width-1-3 uk-padding-small" style={{'maxHeight': '50vh'}} >
-                    <img className="uk-align-left" style={{"border": "0", "maxWidth": "100%"}} src={denis} alt=""/>
+                <div className="uk-width-1-3@m uk-padding-small uk-visible@m" >
+                    <img style={{"border": "0", "maxHeight": "100%", "maxWidth": "100%"}} src={denis} alt=""/>
                 </div>
-
-                <div className="uk-width-2-3 uk-padding-small">
-                    <p className="uk-h1 uk-margin-small">{data.label}</p>
+                <div className="uk-width-2-3@m uk-padding-small">
+                    <h4 className="uk-h1 uk-margin-small">{data.label}</h4>
                     <hr/>
-                    <p className="uk-h3 uk-margin-small uk-text-italic">"{data.text}"</p>
+                    <p className="uk-margin-small uk-text-italic">"{data.text}"</p>
                 </div>
             </div>
         </div>
