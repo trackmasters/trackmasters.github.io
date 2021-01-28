ea93dba716685ed7de4e347129e2c71dc268fb99
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2021-01-29 00:25:08 +0100

    Racing events disabled buttons

--- src/section/racingEvents/RacingEventCard.js
+++ src/section/racingEvents/RacingEventCard.js
@@ -2,16 +2,33 @@ import React from "react";
 
 import { useMediaQuery } from 'react-responsive';
 
+function RacingEventButton(props) {
+
+	console.log(props.index + " " + props.enabled)
+	return props.disabled ?
+		<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove" disabled>
+			<span className="racing-button-icon" uk-icon={`icon: ${props.icon}; ratio: 2`}/>
+		</button>
+		:
+		<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove"
+				   onMouseOver={props.onMouseOver}>
+			<span className="racing-button-icon" uk-icon={`icon: ${props.icon}; ratio: 2`}/>
+		</button>
+	;
+}
+
 export default function RacingEventCard(props) {
 
-	const [text, setText] = React.useState("default");
+	const [text, setText] = React.useState("");
 	const isS = useMediaQuery({query: '(max-width: 640px)'})
+
+	const disabled = !props.enabled;
 
 	return (
 		isS ?
 		<React.Fragment>
-
-			<div className="uk-grid uk-card-body uk-card-hover uk-grid-collapse uk-child-width-1-2 uk-margin" data-uk-grid="">
+			<div className="uk-grid uk-card-body uk-card-hover uk-grid-collapse uk-child-width-1-2 uk-margin"
+				 data-uk-grid="">
 				<div className="uk-card-media-left uk-cover-container">
 					<div className="uk-card-body">
 						<p className="event-number uk-margin-remove uk-text-center">#{props.index}</p>
@@ -19,22 +36,16 @@ export default function RacingEventCard(props) {
 					</div>
 				</div>
 				<div className="uk-flex-middle">
-					<div className="uk-card-body uk-card-media-top" uk-toggle={`target: #toggle-animation-${props.index}; animation: uk-animation-fade; mode: hover`}>
-						<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove"
-								onMouseOver={() => setText("Informace")}>
-							<span className="racing-button-icon" uk-icon="icon: link; ratio: 2"></span>
-						</button>
-						<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove"
-								onMouseOver={() => setText("Registrace")}>
-							<span className="racing-button-icon" uk-icon="icon: sign-in; ratio: 2"></span>
-						</button>
-						<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove"
-								onMouseOver={() => setText("Výsledky")}>
-							<span className="racing-button-icon" uk-icon="icon: list; ratio: 2"></span>
-						</button>
+					<div className="uk-card-body uk-card-media-top"
+						 uk-toggle={`target: #toggle-animation-${props.index}; animation: uk-animation-fade; mode: hover`}>
+						<RacingEventButton icon="link" onMouseOver={() => setText("Informace")} disabled={disabled}/>
+						<RacingEventButton icon="sign-in" onMouseOver={() => setText("Registrace")} disabled={disabled}/>
+						<RacingEventButton icon="list" onMouseOver={() => setText("Výsledky")} disabled={disabled}/>
 					</div>
 					<div className="uk-card-body uk-card-media-bottom uk-text-center uk-padding-remove">
-						<p id={`toggle-animation-${props.index}`} className="event-detail uk-margin-remove uk-transition-fade uk-transition-opaque" hidden>{text}</p>
+						<p id={`toggle-animation-${props.index}`}
+						   className="event-detail uk-margin-remove uk-transition-fade uk-transition-opaque"
+						   hidden>{text}</p>
 					</div>
 				</div>
 			</div>
@@ -52,18 +63,9 @@ export default function RacingEventCard(props) {
 				<hr className="uk-margin-remove-bottom"/>
 				<div className="event-body uk-card-media-bottom uk-text-center uk-padding-remove"
 					 uk-toggle={`target: #toggle-animation-${props.index}; animation: uk-animation-fade; mode: hover`}>
-					<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove"
-							onMouseOver={() => setText("Informace")}>
-						<span className="racing-button-icon" uk-icon="icon: link; ratio: 2"></span>
-					</button>
-					<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove"
-							onMouseOver={() => setText("Registrace")}>
-						<span className="racing-button-icon" uk-icon="icon: sign-in; ratio: 2"></span>
-					</button>
-					<button className="racing-button uk-button uk-button-default uk-width-1-3 uk-margin-remove uk-padding-remove"
-							onMouseOver={() => setText("Výsledky")}>
-						<span className="racing-button-icon" uk-icon="icon: list; ratio: 2"></span>
-					</button>
+					<RacingEventButton icon="link" onMouseOver={() => setText("Informace")} disabled={disabled}/>
+					<RacingEventButton icon="sign-in" onMouseOver={() => setText("Registrace")} disabled={disabled}/>
+					<RacingEventButton icon="list" onMouseOver={() => setText("Výsledky")} disabled={disabled}/>
 				</div>
 				<hr className="uk-margin-remove-top uk-margin-remove-bottom"/>
 				<div className="uk-flex uk-flex-middle uk-flex-center" style={{'height': '50px'}}>
--- src/section/racingEvents/RacingEvents.js
+++ src/section/racingEvents/RacingEvents.js
@@ -28,7 +28,7 @@ class RacingEvents extends React.Component {
 				{
 					this.state.json.map(event => {
 						index++;
-						return <RacingEventCard key={index} date={event.date} place={event.place} index={index}/>
+						return <RacingEventCard key={index} date={event.date} enabled={event.enabled} index={index}/>
 					})
 				}
 			</RacingEventsContainer>
