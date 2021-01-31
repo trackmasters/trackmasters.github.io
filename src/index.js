93063925ab4a46acbedd04780f070b826740a068
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2021-01-31 23:20:06 +0100

    Webpack notification

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 UIkit.use(Icons);
-UIkit.notification('Webpack UIKit 3 bundle loaded!');
+
+if (process.env.ENVIRONMENT !== 'prod') {
+    UIkit.notification('Webpack bundle loaded!');
+}
+
 window.UIkit = UIkit;
 
 ReactDOM.render(<App/>, document.getElementById("root"));
