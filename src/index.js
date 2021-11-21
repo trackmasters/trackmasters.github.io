dc843f093228cf6cc1071b1a738c12b48514d391
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2021-11-21 15:01:30 +0100

    Mixpanel test

--- src/index.js
+++ src/index.js
@@ -9,13 +9,19 @@ import './favicon.ico';
 
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import mixpanel from 'mixpanel-browser';
 
 UIkit.use(Icons);
 
 if (process.env.ENVIRONMENT !== 'production') {
     console.log(process.env.ENVIRONMENT)
     UIkit.notification('Webpack bundle loaded!');
+    mixpanel.init(process.env.MIXPANEL_TOKEN,{api_host: "https://api.mixpanel.com", ignore_dnt: true, debug: true});
+} else {
+    mixpanel.init(process.env.MIXPANEL_TOKEN,{api_host: "https://api.mixpanel.com", ignore_dnt: true});
 }
+
+mixpanel.track('Init');
 
 window.UIkit = UIkit;
 
--- src/react/navigation/Navigation.js
+++ src/react/navigation/Navigation.js
@@ -2,20 +2,28 @@ import React from "react";
 
 import './Navigation.scss'
 
+import mixpanel from 'mixpanel-browser';
+
 export default function Navigation() {
 	return (
 		<div id='sidenav' uk-sticky className="uk-position-fixed uk-active uk-height-viewport"
 			 data-uk-scrollspy-nav={"closest: li; scroll: true; offset: 0; overflow: true"}>
 			<ul className="uk-nav uk-nav-default tm-nav uk-nav-parent-icon">
-				<li className=""><a href="#Home"><span>ÚVOD</span></a></li>
-				<li className=""><a href="#News"><span>NOVINKY</span></a></li>
-				<li className=""><a href="#AboutRacing"><span>O ZÁVODU</span></a></li>
-				<li className=""><a href="#RacingEvents"><span>TERMÍNY</span></a></li>
-				<li className=""><a href="#Contacts"><span>KONTAKTY</span></a></li>
-				<li className=""><a href="#Director"><span>ŘEDITEL</span></a></li>
-				<li className=""><a href="#Organizers"><span>POŘADATELÉ</span></a></li>
-				<li className=""><a href="#Sponsors"><span>SPONZOŘI</span></a></li>
+				<li className=""><a onClick={() => { track("Home")}} href="#Home"><span>ÚVOD</span></a></li>
+				<li className=""><a onClick={() => { track("News")}} href="#News"><span>NOVINKY</span></a></li>
+				<li className=""><a onClick={() => { track("AboutRacing")}} href="#AboutRacing"><span>O ZÁVODU</span></a></li>
+				<li className=""><a onClick={() => { track("RacingEvents")}} href="#RacingEvents"><span>TERMÍNY</span></a></li>
+				<li className=""><a onClick={() => { track("Contacts")}} href="#Contacts"><span>KONTAKTY</span></a></li>
+				<li className=""><a onClick={() => { track("Director")}} href="#Director"><span>ŘEDITEL</span></a></li>
+				<li className=""><a onClick={() => { track("Organizers")}} href="#Organizers"><span>POŘADATELÉ</span></a></li>
+				<li className=""><a onClick={() => { track("Sponsors")}} href="#Sponsors"><span>SPONZOŘI</span></a></li>
 			</ul>
 		</div>
 	);
+}
+
+function track(event) {
+	console.log("Navigation track START")
+	mixpanel.track(event);
+	console.log("Navigation track END")
 }
