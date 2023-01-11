65426fd38663d15d7acf7b11a39cf74d81de71d7
Nikolas-Charalambidis <Nikolas-Charalambidis@users.noreply.github.com>
2023-01-11 15:22:58 +0000

    Node 16

---
+++ vendors.mdast-util-gfm-strikethrough.10f11d5d4fe3d9d1c48d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{302:function(e,n){n.canContainEols=["delete"],n.enter={strikethrough:function(e){this.enter({type:"delete",children:[]},e)}},n.exit={strikethrough:function(e){this.exit(e)}}},307:function(e,n,t){var r=t(100);function i(e,n,t){var i=t.enter("emphasis"),o=r(e,t,{before:"~",after:"~"});return i(),"~~"+o+"~~"}n.unsafe=[{character:"~",inConstruct:"phrasing"}],n.handlers={delete:i},i.peek=function(){return"~"}}}]);
+//# sourceMappingURL=vendors.mdast-util-gfm-strikethrough.10f11d5d4fe3d9d1c48d.js.map
